4b36e13a36e6326d0b03b03f4ee9e158116b63b6
maruf <dev.maruf112@gmail.com>
2022-12-26 13:48:46 +0600

    login and register button on navbar

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -7,8 +7,13 @@ const Navbar = () => {
       <li>
         <Link to={"/home"}>Home</Link>
       </li>
-      <li>
+      <l>
         <Link to={"/aboutUs"}>About Us</Link>
+        <li>
+        <Link to={"/login"}>Login</Link>
+      </li>
+      <li>
+        <Link to={"/register"}>Register</Link>
       </li>
     </>
   );
