b8b6b24bcf21226eff9affe3e1705d7337fdcb78
maruf <dev.maruf112@gmail.com>
2022-12-26 09:55:27 +0600

    About me done

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import image from "../../../src/images/passport_pic.png";
+import "./AboutUs.css";
 const AboutUs = () => {
-    return (
-        <div>
-            <h2>About Us</h2>
-        </div>
-    );
+  return (
+    <div className="max-w-7xl mx-auto mt-10 flex justify-around flex-wrap">
+      <div className="mb-5">
+        <img src={image} alt="" />
+      </div>
+      <div className="flex flex-col lg:items-center ">
+        <p className="text-2xl mb-1 text-justify font-bold">Maruf Hossain</p>
+        <p className="about-me text-justify">
+          I am Maruf Hossain. I am Junior Web Developer. I have skills on
+          Javascript,React,NodeJs,MongoDB,ExpressJs,Firebase etc. I have taken
+          an online course from Programming Hero on Web Development.
+        </p>
+      </div>
+    </div>
+  );
 };
-
 export default AboutUs;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-neutral text-neutral-content">
+    <footer className="footer p-10 bg-neutral text-neutral-content mt-5">
       <div>
         <span className="footer-title">Services</span>
         <a className="link link-hover">Branding</a>
